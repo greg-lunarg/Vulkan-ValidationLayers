dec829d6337ea00e10430f6f433c115263e2eb2c
Tony-LunarG <tony@lunarg.com>
2020-04-29 13:02:51 -0600

    tests: Make api checks consistent
    Change-Id: I4e88f39c0f233fdce289382088d069d1dff085fb

--- tests/layer_validation_tests.cpp
+++ tests/layer_validation_tests.cpp
@@ -1027,10 +1027,8 @@ uint32_t VkLayerTest::SetTargetApiVersion(uint32_t target_api_version) {
     return m_target_api_version;
 }
 uint32_t VkLayerTest::DeviceValidationVersion() {
-    // The validation layers, assume the version we are validating to is the apiVersion unless the device apiVersion is lower
-    VkPhysicalDeviceProperties props;
-    GetPhysicalDeviceProperties(&props);
-    return std::min(m_target_api_version, props.apiVersion);
+    // The validation layers assume the version we are validating to is the apiVersion unless the device apiVersion is lower
+    return std::min(m_target_api_version, pdp().apiVersion);
 }
 
 bool VkLayerTest::LoadDeviceProfileLayer(
